b59dba7ed344a06fafb705645e32fbb975fbefc7
Riya Karunde <riyakarunde168@gmail.com>
2026-05-10 21:21:43 +0530

    Fix BlurBlob import case for Vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Work from "./components/Work/Work";
 import Education from "./components/Education/Education";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
-import BlurBlob from './components/Blurblob';
+import BlurBlob from './components/BlurBlob';
 
 const App = () => {
   return (
